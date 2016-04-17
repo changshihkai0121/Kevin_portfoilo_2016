990f12d7b4dda9a4bb5173d36aac2502003d2856
SHihKaI <kevin20020121@gmail.com>
2016-04-17 16:15:33 +0800

    include fonts & change style

--- js/common.js
+++ js/common.js
@@ -1,15 +1,25 @@
-//portfoilo of Shih-Kai
-var windowWidth = $(window).width();
-var windowHeight = $(window).height();
+/**
+=============================================================================
+ portfoilo of Shih-Kai of jquery + javascript
+=============================================================================
+**/
+
+var _windowWidth, _windowHeight;
+
+// count size
+function objectSize() {
+    _windowWidth = $(window).width();
+    _windowHeight = $(window).height();
+}
 
 //Checking for Retina Devices
 function isRetina() {
     var query = '(-webkit-min-device-pixel-ratio: 1.5),\
-				(min--moz-device-pixel-ratio: 1.5),\
-				(-o-min-device-pixel-ratio: 3/2),\
-				(min-device-pixel-ratio: 1.5),\
-				(min-resolution: 144dpi),\
-				(min-resolution: 1.5dppx)';
+                (min--moz-device-pixel-ratio: 1.5),\
+                (-o-min-device-pixel-ratio: 3/2),\
+                (min-device-pixel-ratio: 1.5),\
+                (min-resolution: 144dpi),\
+                (min-resolution: 1.5dppx)';
     if (window.devicePixelRatio > 1 || (window.matchMedia && window.matchMedia(query).matches)) {
         return true;
     } else {
@@ -25,35 +35,29 @@ function isDevices() {
         isWebOS = /webos/i.test(navigator.userAgent.toLowerCase()),
         isWindowsPhone = /windows phone/i.test(navigator.userAgent.toLowerCase());
     if (isiDevice || isAndroid || isBlackBerry || isWebOS || isWindowsPhone) {
-        return true; 
+        return true;
     } else {
         false
     }
 }
 
-// Jquery auto run function began
+/* Jquery auto run function began */
 $(function() {
+    objectSize(); // include object size
     if (isRetina()) {
-    	// all classname is images-icon change
-        $('.images-icon').each(function(){
-            $(this).attr('src',$(this).attr('src').replace('.png','.svgz'));
+        $('.images-icon').each(function() { // all class-name: images-icon change retina use pic
+            $(this).attr('src', $(this).attr('src').replace('.png', '.svg'));
         });
-
-    } else {
-
-    };
+    } else {};
 });
-/*	Jquery auto run function end
---------------------------------
-*/
+/*  Jquery auto run function end */
 
-// window resize action began
-$(window).resize(function() {});
-/* window resize action end
----------------------------
-*/
+/* window resize action began */
+$(window).resize(function() {
+    objectSize();// include object size
+});
+/* window resize action end */
 
-// window scroll animate began
+/* window scroll animate began */
 $(window).scroll(function() {});
-/* window scroll animate end
- */
+/* window scroll animate end */
