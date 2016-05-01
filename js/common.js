7f1919877ab3a0b7b4f389b2a72d3cf26bbcee65
SHihKaI <kevin20020121@gmail.com>
2016-05-02 00:39:03 +0800

    push 1. add animate 2. add loading animation 3. add pace js  script

--- js/common.js
+++ js/common.js
@@ -41,21 +41,89 @@ function isDevices() {
     }
 }
 
+
 /* Jquery auto run function began */
 $(function() {
     objectSize(); // include object size
+    $('body').append('<div id=' + '\"paceDiv\"' + '></div><div class=' + '\"paceBox\"' + '><div class=' + '\"loader\"' + '><div class=' + '\"dot\"' + '></div><div class=' + '\"dot\"' + '></div><div class=' + '\"dot\"' + '></div> <div class=' + '\"dot\"' + '></div><div class=' + '\"dot\"' + '></div></div>');
+    //append loading site
+    paceOptions = {
+        ajax: false, // disabled
+        document: false, // disabled
+        eventLag: false, // disabled
+        elements: {
+            selectors: ['.container']
+        }
+    };
+    Pace.on("done", function() {
+        $('#paceDiv').fadeOut();
+        $('.paceBox').fadeOut();
+        setTimeout(function() {
+            $('#paceDiv').remove();
+        }, 3000);
+    });
     if (isRetina()) {
         $('.images-icon').each(function() { // all class-name: images-icon change retina use pic
             $(this).attr('src', $(this).attr('src').replace('.png', '.svg'));
         });
     } else {};
+    //mouse enter Box action --  jquery Mouseenter
+    $('.touch-right').click(function() {
+        var $beforeBox = $('#content-Before');
+        touchBoxWidth = $('#touchBox').width();
+        rightBox_Width = $(this).width();
+        percentWidth = rightBox_Width / touchBoxWidth;
+        if (percentWidth <= 0.9) {
+            $beforeBox.css('z-index', '990');
+            $beforeBox.animate({ 'width': '100%' });
+            $(this).css('width', '100%');
+            $('.touch-left').css('width', 0);
+            $('.face-right').css({ 'width': '100%' });
+            $('.move-Icon p').animate({ 'width': '100%' });
+            $beforeBox.find('.fontTime p').delay(800).animate({ 'opacity': 1 });
+        } else {
+            $beforeBox.animate({ 'width': '50%' });
+            $(this).css('width', '50%');
+            $('.touch-left').css('width', '50%');
+            $('.face-right').css({ 'width': '197%' });
+            $('.move-Icon p').animate({ 'width': '60%' });
+            $beforeBox.find('.fontTime p').animate({ 'opacity': 0 });
+        }
+    });
+    $('.touch-left').click(function() {
+        var $beforeBox = $('#content-After');
+        touchBoxWidth = $('#touchBox').width();
+        rightBox_Width = $(this).width();
+        percentWidth = rightBox_Width / touchBoxWidth;
+        if (percentWidth <= 0.9) {
+            $beforeBox.css('z-index', '991');
+            $beforeBox.animate({ 'width': '100%' });
+            $(this).css('width', '100%');
+            $('.touch-right').css('width', 0);
+            $('.face-left').css({ 'width': '100%' });
+            $('.move-Icon p').animate({ 'width': '100%' });
+            $beforeBox.find('.fontTime p').delay(800).animate({ 'opacity': 1 });
+        } else {
+            $beforeBox.css('z-index', '990');
+            $beforeBox.animate({ 'width': '50%' });
+            $(this).css('width', '50%');
+            $('.touch-right').css('width', '50%');
+            $('.face-left').css({ 'width': '203%' });
+            $('.move-Icon p').animate({ 'width': '60%' });
+            $beforeBox.find('.fontTime p').animate({ 'opacity': 0 });
+        }
+    });
     //menu btn click animation
     $('.menu-btn').click(function() {
         $('.menu-container').fadeIn();
     });
-    //menu btn click animation end
+    $('.closeMenu-Btn').click(function() {
+        $('.menu-container').fadeOut();
+    });
 });
 /*  Jquery auto run function end */
+
+
 
 /* window resize action began */
 $(window).resize(function() {
