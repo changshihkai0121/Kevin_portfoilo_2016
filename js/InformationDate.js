b51f34f1de63a1fdc801ec66b052c99b2da2115f
SHihKaI <kevin20020121@gmail.com>
2016-05-12 00:18:16 +0800

    1. fix slideshow script 2. fix slideshow scss style

--- js/InformationDate.js
+++ js/InformationDate.js
@@ -26,70 +26,103 @@ var beforeData = [
 	'project_id':'p00',
 	'cat_id':'i01',
 	'project_title':'卡厝味',
-	'project_info':'',
 	'project_mainPic':'/images/i01@1x.jpg'
 },
 {
 	'project_id':'p01',
 	'cat_id':'i01',
 	'project_title':'卡厝味 － 產品介紹',
-	'project_info':'',
 	'project_mainPic':'/images/i01@1x.jpg'
 },
 {
 	'project_id':'p02',
 	'cat_id':'i01',
 	'project_title':'卡厝味 － App介紹',
-	'project_info':'',
 	'project_mainPic':'/images/i01@1x.jpg'
 },
 {
 	'project_id':'p03',
 	'cat_id':'i02',
 	'project_title':'3D建模',
-	'project_info':'',
 	'project_mainPic':'/images/i01@1x.jpg'
 },
 {
 	'project_id':'p04',
 	'cat_id':'i02',
 	'project_title':'電子報',
-	'project_info':'',
 	'project_mainPic':'/images/i01@1x.jpg'
 },
 {
 	'project_id':'p04',
 	'cat_id':'i02',
 	'project_title':'月曆設計',
-	'project_info':'',
 	'project_mainPic':'/images/i01@1x.jpg'
 },
 {
 	'project_id':'p05',
 	'cat_id':'i02',
 	'project_title':'海報設計',
-	'project_info':'',
 	'project_mainPic':'/images/i01@1x.jpg'
 },
 {
 	'project_id':'p06',
 	'cat_id':'i02',
 	'project_title':'MV動畫',
-	'project_info':'',
 	'project_mainPic':'/images/i01@1x.jpg'
 },
 {
-	'project_id':'p06',
+	'project_id':'p07',
 	'cat_id':'i02',
 	'project_title':'插畫',
-	'project_info':'',
 	'project_mainPic':'/images/i01@1x.jpg'
 },
 {
-	'project_id':'p06',
+	'project_id':'p08',
 	'cat_id':'i02',
 	'project_title':'手繪',
-	'project_info':'',
 	'project_mainPic':'/images/i01@1x.jpg'
 }
 ];
+var beforeInformation = [
+{
+	'project_id':'p01',
+	'project_title':'AAA',
+	'project_how':'',
+	'project_what':'',
+	'project_why':''
+},
+{
+	'project_id':'p01',
+	'project_title':'AAA',
+	'project_how':'',
+	'project_what':'',
+	'project_why':''
+},
+{
+	'project_id':'p01',
+	'project_title':'AAA',
+	'project_how':'',
+	'project_what':'',
+	'project_why':''
+},
+{
+	'project_id':'p01',
+	'project_title':'AAA',
+	'project_how':'',
+	'project_what':'',
+	'project_why':''
+},
+{
+	'project_id':'p01',
+	'project_title':'AAA',
+	'project_how':'',
+	'project_what':'',
+	'project_why':''
+},
+{
+	'project_id':'p01',
+	'project_title':'AAA',
+	'project_how':'',
+	'project_what':'',
+	'project_why':''
+},{},{},{},{},{},{},{},{}];
--- js/before.js
+++ js/before.js
@@ -1,9 +1,9 @@
+//post choose-cat option information
+var $chooseOption = $('#choose-cat'),
+    $slideshowList = $('.slideshowInfo'),
+    beforehtml_text = '',
+    before_default = '';
 $(function() {
-    //post choose-cat option information
-    var $chooseOption = $('#choose-cat'),
-    	$slideshowList = $('.slideshowInfo'),
-        beforehtml_text = '',
-        before_default = '';
     //past option
     for (i = 0; i < beforeIndex.length; i++) {
         beforehtml_text += '<option value=' + beforeIndex[i].index_id + '>' + beforeIndex[i].index_title;
@@ -11,37 +11,77 @@ $(function() {
         $chooseOption.html(beforehtml_text);
     }
     for (var a = 0; a < beforeData.length; a++) {
-    	before_default += '<li class=' + '\"slideshow_onecontent\"' + '><img src=' + beforeData[a].project_mainPic + ' alt=' + beforeData[a].project_title + ' /></li>';
-    	$slideshowList.html(before_default);
+        before_default += '<li class=' + '\"slideshow-onecontent\"' + '><img src=' + beforeData[a].project_mainPic + ' alt=' + beforeData[a].project_title + ' /></li>';
+        $slideshowList.html(before_default);
     };
-    // choose one and selected information
-    $chooseOption.on('change', function() {
-        var Value = $(this).find('option:selected').val(),
-            templatecontent = '';
-        if (Value == 'i00') {
-            for (var i = 0; i < beforeData.length; i++) {
-                templatecontent += '<li class=' + '\"slideshow_onecontent\"' + '><img src=' + beforeData[i].project_mainPic + ' alt=' + beforeData[i].project_title + ' /></li>';
-                $slideshowList.html(templatecontent);
-            };
-        } else if (Value == 'i01') {
-            for (var i = 0; i < beforeData.length; i++) {
-                if (Value == beforeData[i].cat_id) {
-                    templatecontent += '<li class=' + '\"slideshow_onecontent\"' + '><img src=' + beforeData[i].project_mainPic + ' alt=' + beforeData[i].project_title + ' /></li>';
-                    $slideshowList.html(templatecontent);
-                }
-            };
-        } else if (Value == 'i02') {
-            for (var i = 0; i < beforeData.length; i++) {
-                if (Value == beforeData[i].cat_id) {
-                    templatecontent += '<li class=' + '\"slideshow_onecontent\"' + '><img src=' + beforeData[i].project_mainPic + ' alt=' + beforeData[i].project_title + ' /></li>';
-                    $slideshowList.html(templatecontent);
-                }
-            };
-        } else {}
-    });
-    // if (isRetina()) {
-    //     $('.slideshow_onecontent').each(function() {
-    //         $(this).find('img').attr('src', $(this).attr('src').replace('@1x', '@2x'));
-    //     });
-    // }
+    slideshow_isRetina();
 });
+
+// choose one and selected information
+$chooseOption.on('change', function() {
+    var Value = $(this).find('option:selected').val(),
+        slideshowContentHeight = $('.slideshow-onecontent').height(),
+        templatecontent = '';
+    if (Value == 'i00') {
+        for (var i = 0; i < beforeData.length; i++) {
+            templatecontent += '<li class=' + '\"slideshow-onecontent\"' + '><img src=' + beforeData[i].project_mainPic + ' alt=' + beforeData[i].project_title + ' /></li>';
+            $slideshowList.html(templatecontent);
+        };
+        $('.slideshowInfo').animate({ 'bottom': slideshowContentHeight * 0 });
+        slideshow_isRetina();
+    } else if (Value == 'i01') {
+        for (var i = 0; i < beforeData.length; i++) {
+            if (Value == beforeData[i].cat_id) {
+                templatecontent += '<li class=' + '\"slideshow-onecontent\"' + '><img src=' + beforeData[i].project_mainPic + ' alt=' + beforeData[i].project_title + ' /></li>';
+                $slideshowList.html(templatecontent);
+            }
+        };
+        $('.slideshowInfo').animate({ 'bottom': slideshowContentHeight * 0 });
+        slideshow_isRetina();
+    } else if (Value == 'i02') {
+        for (var i = 0; i < beforeData.length; i++) {
+            if (Value == beforeData[i].cat_id) {
+                templatecontent += '<li class=' + '\"slideshow-onecontent\"' + '><img src=' + beforeData[i].project_mainPic + ' alt=' + beforeData[i].project_title + ' /></li>';
+                $slideshowList.html(templatecontent);
+            }
+        };
+        $('.slideshowInfo').animate({ 'bottom': slideshowContentHeight * 0 });
+        slideshow_isRetina();
+    } else {}
+});
+
+//slideshow
+var slidepage = 0;
+
+function slideshowBtn(btnID) {
+    var slideshowContentHeight = $('.slideshow-onecontent').height(),
+        slideshowInfobox = $('.slideshowInfo').height(),
+        datalength = $('.slideshow-onecontent').length;
+    if (btnID == 'slideshowPrivate') {
+        if (datalength >= slidepage) {
+            slidepage = slidepage + 1;
+            if (slidepage < datalength) {
+                $('.slideshowInfo').animate({ 'bottom': slideshowContentHeight * slidepage });
+            } else {
+                slidepage = 0;
+                $('.slideshowInfo').animate({ 'bottom': slideshowContentHeight * slidepage });
+            }
+        }
+    } else if (btnID == 'slideshownext') {
+        if (slidepage <= 0) {
+            slidepage = (datalength - 1);
+            $('.slideshowInfo').animate({ 'bottom': slideshowContentHeight * slidepage });
+        } else {
+            slidepage = slidepage - 1;
+            $('.slideshowInfo').animate({ 'bottom': slideshowContentHeight * slidepage });
+        }
+    }
+}
+
+function slideshow_isRetina() {
+    if (isRetina()) {
+        $('.slideshow-onecontent').each(function() {
+            $(this).find('img').attr('src', $(this).find('img').attr('src').replace('1x', '2x'));
+        });
+    }
+}
